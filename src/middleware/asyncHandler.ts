199ade4991b3ab9517cefdbc456ee447f0ceb8d6
Avet Badalyan <avetbadalyan@gmail.com>
2024-09-03 14:49:22 +0400

    Update asyncHandler.ts

--- src/middleware/asyncHandler.ts
+++ src/middleware/asyncHandler.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
-const asyncHandler =
-  (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
-  (req: Request, res: Response, next: NextFunction) =>
-    Promise.resolve(fn(req, res, next)).catch(next);
+const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next);
+  };
+};
 
 export default asyncHandler;
+
